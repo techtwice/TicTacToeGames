d9aa3fa45971831572725d730a1b8b841b3509a7
techtwice <tectwice@gmail.com>
2020-04-21 03:41:56 +0600

    playGames Function Design

--- app/src/main/java/com/techtwice/tictactoegames/MainActivity.kt
+++ app/src/main/java/com/techtwice/tictactoegames/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.techtwice.tictactoegames
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    var activePlayer = 1
+
+    var player1 = ArrayList<Int>()
+    var player2 = ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,30 +28,50 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         bt8.setOnClickListener(this)
         bt9.setOnClickListener(this)
 
-
     }
 
     override fun onClick(view: View) {
 
         val btSelected = view as Button
-        var btCollID = 0
+        var collId = 0
 
         when (btSelected.id) {
-            R.id.bt1 -> btCollID = 1
-            R.id.bt2 -> btCollID = 2
-            R.id.bt3 -> btCollID = 3
-            R.id.bt4 -> btCollID = 4
-            R.id.bt5 -> btCollID = 5
-            R.id.bt6 -> btCollID = 6
-            R.id.bt7 -> btCollID = 7
-            R.id.bt8 -> btCollID = 8
-            R.id.bt9 -> btCollID = 9
+            R.id.bt1 -> collId = 1
+            R.id.bt2 -> collId = 2
+            R.id.bt3 -> collId = 3
+            R.id.bt4 -> collId = 4
+            R.id.bt5 -> collId = 5
+            R.id.bt6 -> collId = 6
+            R.id.bt7 -> collId = 7
+            R.id.bt8 -> collId = 8
+            R.id.bt9 -> collId = 9
         }
 
-        //   Log.d("btClick:", btSelected.id.toString())
-        //   Log.d("btClick: btCollID:", btCollID.toString())
-        Toast.makeText(this, "btClick:" + btSelected.id.toString(), Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "btClick: btCollID:" + btCollID.toString(), Toast.LENGTH_LONG).show()
+        // Log.d("btClick:", btSelected.id.toString())
+        // Log.d("btClick: collId:", collId.toString())
+        // Toast.makeText(this, "btClick:" + btSelected.id.toString(), Toast.LENGTH_LONG).show()
+        // Toast.makeText(this, "btClick: collId:" + collId.toString(), Toast.LENGTH_LONG).show()
+
+        playGames(collId, btSelected)
 
     }
+
+    private fun playGames(collId: Int, btSelected: Button) {
+
+        if (activePlayer == 1) {
+            btSelected.text = "X"
+            btSelected.setBackgroundResource(R.color.blue)
+            player1.add(collId)
+            activePlayer = 2
+        } else {
+            btSelected.text = "O"
+            btSelected.setBackgroundResource(R.color.darkGreen)
+            player2.add(collId)
+            activePlayer = 1
+        }
+
+        btSelected.isEnabled = false
+    }
+
+
 }
